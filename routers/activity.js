e8aea2697180f5f878f05da4203324cb6b89f927
huxiang <517626980@qq.com>
2018-08-16 17:40:49 +0800

    use react

--- routers/activity.js
+++ routers/activity.js
@@ -1,9 +1,28 @@
 import router from 'koa-router';
+import ReactDOMServer from 'react-dom/server';
+import React, {PureComponent} from 'react';
+
+class MyComponent extends PureComponent {
+    render () {
+        return <div>This dom is render by server. </div>;
+    }
+}
 
 const homeRouter = router();
 
 homeRouter.get('/activity', async(ctx, next) => {
-    ctx.body = {result: 'good'};
+    ctx.body = `
+        <!DOCTYPE html>
+        <html lang="en" dir="ltr">
+          <head>
+            <meta charset="utf-8">
+            <title></title>
+          </head>
+          <body>
+            ${ReactDOMServer.renderToString(<MyComponent/>)}
+          </body>
+        </html>
+    `;
 });
 
 export default homeRouter;
